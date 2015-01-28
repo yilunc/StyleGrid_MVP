49c7a6831e61840eca7217367e788b65cbe6b131
yilun <yilunc@hotmail.com>
2015-01-27 23:05:09 -0800

    Initial Commit

---
+++ StyleGrid_MVP_0.01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StyleGrid_MVP_0.01
+//
+//  Created by Yilun Chen on 2015-01-09.
+//  Copyright (c) 2015 Yilun Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ StyleGrid_MVP_0.01Tests/StyleGrid_MVP_0_01Tests.swift
@@ -0,0 +1,36 @@
+//
+//  StyleGrid_MVP_0_01Tests.swift
+//  StyleGrid_MVP_0.01Tests
+//
+//  Created by Yilun Chen on 2015-01-09.
+//  Copyright (c) 2015 Yilun Chen. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class StyleGrid_MVP_0_01Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
